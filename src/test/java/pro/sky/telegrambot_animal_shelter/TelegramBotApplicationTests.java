b6b38dbe30061e4d41e83aa95adf479c8f3616fc
Юнусов Азат <kill-azat@mail.ru>
2023-12-15 12:44:55 +0300

    Первый коммит по телеграм боту (приют для животных)

---
+++ src/test/java/pro/sky/telegrambot_animal_shelter/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot_animal_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
